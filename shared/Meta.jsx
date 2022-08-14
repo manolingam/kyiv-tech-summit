57c91b7f15fb8bd9cf1fe1e7ead9921b715c9ca5
Manolingam N <manolingam1996@gmail.com>
2022-08-14 21:35:08 +0530

    metadata additions

--- shared/Meta.jsx
+++ shared/Meta.jsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+import logo from '../public/assets/logos/kyiv_tech_summit.png';
+
 export const Meta = () => {
   return (
     <>
@@ -11,6 +13,15 @@ export const Meta = () => {
           property='og:description'
           content='A web3 hackathon for Ukraine.'
         />
+        <meta property='og:image' content={logo} />
+        <meta property='og:url' content='https://www.kyivtechsummit.com/' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content='Kyiv Tech Summit' />
+        <meta
+          name='twitter:description'
+          content='A web3 hackathon for Ukraine.'
+        />
+        <meta name='twitter:image' content={logo} />
         <meta property='og:type' content='website' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
