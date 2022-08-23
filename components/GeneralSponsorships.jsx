707e332cb5a0177ad2ec17a0d9170b6b2986cbfd
Manolingam N <manolingam1996@gmail.com>
2022-08-23 21:59:59 +0530

    sponsorship table update

--- components/GeneralSponsorships.jsx
+++ components/GeneralSponsorships.jsx
@@ -53,36 +53,6 @@ export const GeneralSponsorships = () => {
             >
               Supportive Yellow
             </Th>
-            <Th
-              fontFamily='figTree'
-              fontSize='1.2rem'
-              bg='#ECB390'
-              color='black'
-              textTransform='none'
-              textAlign='center'
-            >
-              Bronze
-            </Th>
-            <Th
-              fontFamily='figTree'
-              fontSize='1.2rem'
-              bg='#748DA6'
-              color='white'
-              textTransform='none'
-              textAlign='center'
-            >
-              Silver
-            </Th>
-            <Th
-              fontFamily='figTree'
-              fontSize='1.2rem'
-              bg='#E6B325'
-              color='white'
-              textTransform='none'
-              textAlign='center'
-            >
-              Gold
-            </Th>
           </Tr>
         </Thead>
         <Tbody fontFamily='openSans'>
@@ -90,113 +60,66 @@ export const GeneralSponsorships = () => {
             <Td>Logo across all digital campaigns</Td>
             <Td textAlign='center'>Medium</Td>
             <Td textAlign='center'>Small</Td>
-            <Td textAlign='center'>Small</Td>
-            <Td textAlign='center'>Medium</Td>
-            <Td textAlign='center'>Large</Td>
           </Tr>
           <Tr>
             <Td>Logo on event / stage banners</Td>
             <Td textAlign='center'>Medium</Td>
             <Td textAlign='center'>Small</Td>
-            <Td textAlign='center'>Small</Td>
-            <Td textAlign='center'>Medium</Td>
-            <Td textAlign='center'>Large</Td>
           </Tr>
           <Tr>
             <Td>Booth in sponsor area</Td>
             <Td textAlign='center'>Medium</Td>
             <Td textAlign='center'>Small</Td>
-            <Td textAlign='center'>Small</Td>
-            <Td textAlign='center'>Medium</Td>
-            <Td textAlign='center'>Large</Td>
           </Tr>
           <Tr>
             <Td>Pre & during event/hackathon comms</Td>
             <Td textAlign='center'>+</Td>
             <Td textAlign='center'>+</Td>
-            <Td textAlign='center'>+</Td>
-            <Td textAlign='center'>+</Td>
-            <Td textAlign='center'>+</Td>
-          </Tr>
-          <Tr>
-            <Td>Branded videocast</Td>
-            <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>Published across all socials</Td>
           </Tr>
           <Tr>
             <Td>Speaker invitations</Td>
             <Td textAlign='center'>2</Td>
             <Td textAlign='center'>1</Td>
-            <Td textAlign='center'>1</Td>
-            <Td textAlign='center'>1</Td>
-            <Td textAlign='center'>2</Td>
           </Tr>
           <Tr>
             <Td>IRL conference pass</Td>
             <Td textAlign='center'>5</Td>
             <Td textAlign='center'>2</Td>
-            <Td textAlign='center'>2</Td>
-            <Td textAlign='center'>4</Td>
-            <Td textAlign='center'>8</Td>
           </Tr>
           <Tr>
             <Td>Hosted livestreams</Td>
             <Td textAlign='center'>2</Td>
             <Td textAlign='center'>1</Td>
-            <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>1</Td>
-            <Td textAlign='center'>Up to 2</Td>
           </Tr>
           <Tr>
             <Td>Maximum # of Bounties in Hackathon</Td>
             <Td textAlign='center'>4</Td>
             <Td textAlign='center'>2</Td>
-            <Td textAlign='center'>1</Td>
-            <Td textAlign='center'>2</Td>
-            <Td textAlign='center'>4</Td>
           </Tr>
           <Tr>
             <Td>Custom Promo Designs/POAPs</Td>
             <Td textAlign='center'>+</Td>
             <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>+</Td>
-            <Td textAlign='center'>+</Td>
           </Tr>
           <Tr>
             <Td>Dedicated Partner Support</Td>
             <Td textAlign='center'>+</Td>
             <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>+</Td>
-            <Td textAlign='center'>+</Td>
           </Tr>
           <Tr>
             <Td>Community Growth {'>'} Access to Participants Info</Td>
             <Td textAlign='center'>+</Td>
             <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>+</Td>
-            <Td textAlign='center'>+</Td>
           </Tr>
           <Tr>
             <Td>Promotion to 50k+ tech talent of Ukraine</Td>
             <Td textAlign='center'>+</Td>
             <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>+</Td>
           </Tr>
           <Tr>
             <Td>Promotion to 1.5M+ Ukrainians via Media Partners</Td>
             <Td textAlign='center'>+</Td>
             <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>-</Td>
-            <Td textAlign='center'>+</Td>
           </Tr>
           <Tr>
             <Th
@@ -225,33 +148,6 @@ export const GeneralSponsorships = () => {
             >
               1/10
             </Th>
-            <Th
-              fontFamily='figTree'
-              textTransform='none'
-              fontSize='1.2rem'
-              fontWeight='bold'
-              textAlign='center'
-            >
-              1/7
-            </Th>
-            <Th
-              fontFamily='figTree'
-              textTransform='none'
-              fontSize='1.2rem'
-              fontWeight='bold'
-              textAlign='center'
-            >
-              3/5
-            </Th>
-            <Th
-              fontFamily='figTree'
-              textTransform='none'
-              fontSize='1.2rem'
-              fontWeight='bold'
-              textAlign='center'
-            >
-              2/3
-            </Th>
           </Tr>
         </Tbody>
         <Tfoot>
@@ -278,42 +174,12 @@ export const GeneralSponsorships = () => {
             <Th
               fontFamily='figTree'
               fontSize='1.2rem'
-              bg='#ECB390'
+              bg='#ffd700'
               color='black'
               textTransform='none'
               textAlign='center'
             >
               $10,000
-            </Th>
-            <Th
-              fontFamily='figTree'
-              fontSize='1.2rem'
-              bg='#ECB390'
-              color='black'
-              textTransform='none'
-              textAlign='center'
-            >
-              $10,000
-            </Th>
-            <Th
-              fontFamily='figTree'
-              fontSize='1.2rem'
-              bg='#748DA6'
-              color='white'
-              textTransform='none'
-              textAlign='center'
-            >
-              $25,000
-            </Th>
-            <Th
-              fontFamily='figTree'
-              fontSize='1.2rem'
-              bg='#E6B325'
-              color='white'
-              textTransform='none'
-              textAlign='center'
-            >
-              $50,000
             </Th>
           </Tr>
         </Tfoot>
