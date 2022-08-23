26a4fbe78db3eaa96f55f071bd19fd5164e4aaf6
Manolingam N <manolingam1996@gmail.com>
2022-08-23 23:25:48 +0530

    removed the collapsible section

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { SectionOne } from '../views/SectionOne';
 import { SectionTwo } from '../views/SectionTwo';
 import { SectionThree } from '../views/SectionThree';
 import { SectionFour } from '../views/SectionFour';
-import { SectionFive } from '../views/SectionFive';
+// import { SectionFive } from '../views/SectionFive';
 import { SectionSix } from '../views/SectionSix';
 import { SectionSeven } from '../views/SectionSeven';
 import { SectionEight } from '../views/SectionEight';
@@ -23,7 +23,7 @@ export default function Home() {
 
       <Divider opacity='0.9' borderBottomWidth='2px' maxW='90%' mt='1rem' />
       <SectionFour />
-      <SectionFive />
+      {/* <SectionFive /> */}
       <SectionSix />
 
       <SectionNine />
